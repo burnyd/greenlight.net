9a05a67fcdf21b65a14c4c51d55fe037ce7dfa7c
Daniel Hertzberg <burnyd@gmail.com>
2022-10-03 09:03:00 -0400

    added thingsd

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,12 +6,52 @@ import (
 	"time"
 
 	"github.com/burnyd/greenlight.net/internal/data"
+	"github.com/burnyd/greenlight.net/internal/validator"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier). This struct will be our *target
+	// decode destination*.
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	// Importantly, notice that when we call Decode() we pass a *pointer* to the input
+	// struct as the target decode destination. If there was an error during decoding,
+	// we also use our generic errorResponse() helper to send the client a 400 Bad
+	// Request response containing the error message.
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	// If this returns an error we send the client the error message along with a 400
+	// Bad Request status code, just like before.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		// Use the new badRequestResponse() helper.
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator.
+	v := validator.New()
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
@@ -20,7 +60,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
 		return
 	}
 	movie := data.Movie{
@@ -31,11 +72,9 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		Genres:    []string{"drama", "romance", "war"},
 		Version:   1,
 	}
-	// Create an envelope{"movie": movie} instance and pass it to writeJSON(), instead
-	// of passing the plain movie struct.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
 	}
 }
