99ccb87f4314c7c7decb7a2a4548eaec59098869
Daniel Hertzberg <burnyd@gmail.com>
2022-10-27 10:49:10 -0400

    Added Graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
 	"time"
 
@@ -91,22 +89,27 @@ func main() {
 	// Declare a HTTP server with some sensible timeout settings, which listens on the
 	// port provided in the config struct and uses the servemux we created above as the
 	// handler.
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
+	/*
+		srv := &http.Server{
+			Addr:         fmt.Sprintf(":%d", cfg.port),
+			Handler:      app.routes(),
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+		}*/
 	// Start the HTTP server.
 	// Again, we use the PrintInfo() method to write a "starting server" message at the
 	// INFO level. But this time we pass a map containing additional properties (the
 	// operating environment and server address) as the final parameter.
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
+	//logger.PrintInfo("starting server", map[string]string{
+	//	"addr": srv.Addr,
+	//	"env":  cfg.env,
+	//})
+	//err = srv.ListenAndServe()
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
 	logger.PrintFatal(err, nil)
 }
 
